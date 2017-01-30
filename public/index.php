446feb7179f47eeb8d394a0d7c6e0d03528850d8
Michael Heap <m@michaelheap.com>
2017-01-30 16:39:23 +0000

    Added Twig

--- public/index.php
+++ public/index.php
@@ -7,9 +7,25 @@ require __DIR__.'/../vendor/autoload.php';
 $container = new \Slim\Container;
 $container['settings']['displayErrorDetails'] = true;
 
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__.'/../views', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
 $app = new \Slim\App($container);
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello World");
-    return $response;
+    return $this->view->render($response, 'index.html', [
+        "name" => "Michael"
+    ]);
+});
+$app->get('/weather', function (Request $request, Response $response) {
+    return $this->view->render($response, 'weather.html');
 });
 $app->run();
