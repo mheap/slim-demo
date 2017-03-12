7426c1fb9a98380bde1e5381079dd34bf2f62a72
Michael Heap <m@michaelheap.com>
2017-03-12 02:05:18 +0000

    Add Symfony DI

--- public/index.php
+++ public/index.php
@@ -2,32 +2,17 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use \Flexsounds\Component\SymfonyContainerSlimBridge\ContainerBuilder;
+use \Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
 require __DIR__.'/../vendor/autoload.php';
 
-$container = new \Slim\Container;
-$container['settings']['displayErrorDetails'] = true;
-
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__.'/../views', [
-        'cache' => false
-    ]);
-
-    // Instantiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
-
-    return $view;
-};
-
-$container['controller.home'] = function($container) {
-    return new \Demo\Controller\HomeController($container['view']);
-};
-
-$container['controller.weather'] = function($container) {
-    return new \Demo\Controller\WeatherController($container['view']);
-};
+$container = new ContainerBuilder();
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+$loader->load('services.yml');
 
 $app = new \Slim\App($container);
-$app->get('/', "controller.home:hello");
-$app->get('/weather', "controller.weather:index");
+$app->get('/', 'HomeController:hello');
+$app->get('/weather', "WeatherController:index");
 $app->run();
